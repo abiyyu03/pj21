21de0cda0110f0805dca68c0b50f460cb58d6f10
abiyyu03 <abiyyu652@gmail.com>
2023-01-01 19:09:09 +0700

    add : instruction to config database

--- example.config.php
+++ example.config.php
@@ -1,4 +1,7 @@
 <?php
+# Salin file ini dan ubah namanya jadi config.php
+
+# Sesuaikan isi variable dibawah dengan konfigurasi server/pc lokal 
 $dbHost = 'localhost';
 $dbUsername = '';
 $dbPassword = '';
