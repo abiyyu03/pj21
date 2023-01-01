474d9186dc4ce4471b08979fad5e3dbe3a3c86f6
abiyyu03 <abiyyu652@gmail.com>
2023-01-02 06:53:19 +0700

    fix footer layout

--- components/footer.php
+++ components/footer.php
@@ -1,12 +1,12 @@
 <footer class="footer bottom-0 text-center mt-auto">
     <hr>
-    <div class="d-flex justify-content-between mx-4">
-        <div>
+    <div class="mx-4 row">
+        <div class="col-md-6 col-lg-6 col-sm-12">
             <p class="small text-secondary">Copyleft 2022 - created with <i class="fas fa-heart text-danger"></i> by 
                 <a href="https://github.com/abiyyu03" target="_blank" class="text-secondary">Abiyyu</a> & friends
             </p>
         </div>
-        <div>
+        <div class="col-md-6 col-lg-6 col-sm-12">
             <a href="https://github.com/abiyyu03/jd21" target="_blank" class="text-secondary text-decoration-none small"><i class="fab fa-github"></i> Get Source Code</a>
         </div>
     </div>
