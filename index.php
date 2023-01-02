29e8f3015db70507dce04ecea2f6d4a5b7504645
abiyyu03 <abiyyu652@gmail.com>
2023-01-02 07:35:24 +0700

    update : add meta tags

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ require_once('src/CountData.php');
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Visualisasi data Jumlah Penduduk Provinsi DKI Jakarta Berdasarkan Pekerjaan Per Kelurahan Tahun 2021 ">
+        <meta name="title" content="PJ21">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PJ21 - Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
